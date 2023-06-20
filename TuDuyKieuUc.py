0cea70e5413335cd089283ac6f9b5a61fd9f2a3a
GanDaffe <121656882+GanDaffe@users.noreply.github.com>
2023-06-20 13:20:28 +0700

    Update TuDuyKieuUc.py

--- TuDuyKieuUc.py
+++ TuDuyKieuUc.py
@@ -1,71 +1,48 @@
-def sinh(arr):
-    i = n - 1 #Khoi tao bien i bang n - 1
-    while i >= 0 and arr[i] == 1: #i chạy từ cuối đến đầu list, nếu phát hiện phần tử arr[i] = 1 thì trừ i = i - 1
-        i -= 1 
-    """
-    Ví dụ [0, 0, 0, 0, 0, 0] vì mọi giá trị bằng 0 nên không thực hiện vòng lặp.
-          [0, 0, 0, 0, 0, 1] i bắt đầu chạy từ vị trí thứ 6, đến vt thứ 5 gặp phần tử 0 thì dừng, lúc này i lấy giá trị = 5.
-           1  2  3  4  5  6
-    """
+def asignment(v, path): 
+    if v == end1 or v == end2: 
+        xuli(path)
+    for nx in dic[v]:
+        asignment(nx, path + [nx])
     
-    arr[i] = 1 #chuyển i tại vị trí sau cùng thành giá trị 1
     
-    for idx in range(i + 1, n):
-        arr[idx] = 0
-    """
-    Ví dụ: 
-          [0, 0, 0, 0, 1, 1] là mảng có được sau khi chuyển giá trị ở vị trí thứ 5 của mảng thành 1.
-          lúc này i = 5, ta thực hiện chuyển i + 1 thành 0 được: 
-          [0, 0, 0, 0, 1, 0] được trường hợp tiếp theo của dãy nhị phân
-          
-          Tương tự: 
-          [0, 0, 0, 1, 1, 0]  -> [0, 0, 0, 1, 0, 0]
-          có thể thay chỗ calcTime(arr) thành print(arr) để hiểu rõ cách chuyển hơn
-          
-    """
-      
-def Binary2(): 
-    arr = [0] * n #Khoi tao mang co n so 0
-    while not arr == [1] * n:
-        #print(arr)
-        calcTime(arr) #Tinh ket qua theo ham arr
-        sinh(arr) #Sinh arr moi
-        
-    calcTime(arr)
-    
-def calcTime(bArr): 
-    t1, t2 = 0, 0 
-    for i in range(n): 
-        if bArr[i] == 0:
-            t1 += arr1[i]
-        else: 
-            t2 += arr2[i]
-    
-    tg = max(t1, t2) 
-    list_.append((tg, bArr.copy())) #Các phần tử của list_ là các tuples có dạng (130, [1,0,1,0,0,1])
-    
-
 def xuli(arr): 
-    a1, a2 = "", ""
-    for i in range(n): #[1,0,1,0,1,1] 0 1 2 3 4 5
-        if arr[i] == 0: 
-            a1 += str(i + 1) + ' '
+    a1, a2 = [], []
+    s1, s2 = "", ""
+    for i in range(ep): 
+        if arr[i][2] == 1: 
+            a1.append(arr[i][1])
+            s1 += str(i + 1) + ' '
         else: 
-            a2 += str(i + 1) + ' ' #0 + 1 = 1 -> 1
-    return a1.strip(), a2.strip() #Hàm strip xóa khoảng trống ở đầu và cuối chuỗi: 'hello   ' + strip() == 'hello'
+            a2.append(arr[i][1])
+            s2 += str(i + 1) + ' '
+    
+    tg = max(sum(a1), sum(a2))
+    output.append((tg, s1.strip(), s2.strip()))
+    
 
 with open('INP.txt', 'r') as f: 
     lines = f.readlines()
 
-n = int(lines[0])
-list_ = []
-arr1 = [int(i) for i in lines[1].split(' ')]
-arr2 = [int(i) for i in lines[2].split(' ')]
+ep = int(lines[0])
+arr1 = [(i + 1, int(lines[1].split(' ')[i]), 1) for i in range(len(lines[1].split(' ')))]
+arr2 = [(i + 1, int(lines[2].split(' ')[i]), 2) for i in range(len(lines[2].split(' ')))]
+list_ = arr1 + arr2
+ 
+list_.sort(key = lambda k:k[0])
+dic = {}
 
-Binary2()
+for i in list_: 
+    dic[i] = [t for t in list_ if t[0] == i[0] + 1]
+    
+output = []
 
-w = min(list_, key = lambda k:k[0])
-res1, res2 = xuli(w[1])
+end1 = list_[ep * 2 - 1]
+end2 = list_[ep * 2 - 2] 
+asignment(list_[0], [list_[0]])
+asignment(list_[1], [list_[1]])
 
-with open ('FileOut.O', 'w') as f1: 
-    f1.write('{}\n{}\n{}'.format(w[0],res1,res2)) #Ghi kết quả ra file
+ans = min(output, key = lambda k: k[0])
+print(ans)
+
+with open('OUT.txt', 'w') as f1: 
+    f1.write('{}\n{}\n{}'.format(ans[0], ans[1], ans[2]))
